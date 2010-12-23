805a25044b64f64d947939fb3d03526a3ab5147e
foomagoo <foomagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-23 04:54:44 +0000

    Fixed the ajax requests on the plugin admin page.
    git-svn-id: https://plugins.svn.wordpress.org/plugin-organizer/trunk@325578 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin-organizer.php
+++ plugin-organizer.php
@@ -3,7 +3,7 @@
 Plugin Name: Plugin Organizer
 Plugin URI: http://www.jsterup.com/dev/wordpress/plugin-organizer
 Description: A plugin for specifying the load order of your plugins.
-Version: 0.1
+Version: 0.1.1
 Author: Jeff Sterup
 Author URI: http://www.jsterup.com
 */
@@ -12,7 +12,7 @@ $POAbsPath = WP_PLUGIN_DIR . "/" . plugin_basename(dirname(__FILE__));
 $PluginOrganizer = new Plugin_Organizer();
 
 add_action('admin_menu', array($PluginOrganizer, 'PO_admin_menu'));
-add_filter("plugin_action_links", array($PluginOrganizer, 'PO_plugin_page'), 10, 4);
+add_filter("plugin_action_links", array($PluginOrganizer, 'PO_plugin_page'), 10, 2);
 add_action('admin_head', array($PluginOrganizer, 'PO_ajax_request'));
 add_action('wp_ajax_plugin_organizer',  array($PluginOrganizer, 'PO_save_order'));
 
@@ -58,7 +58,7 @@ class Plugin_Organizer {
 		require_once($POAbsPath . "/tpl/pluginList.php");
 	}
 
-	function PO_plugin_page($buttons, $pluginFile, $arg3, $arg4) {
+	function PO_plugin_page($buttons, $pluginFile) {
 		
 		$plugins = get_option("active_plugins");
 		$newButtonArray = array();
@@ -79,6 +79,7 @@ class Plugin_Organizer {
 			}
 		$orderSelect .= "</select>";
 		$orderSelect .= "<input type=\"hidden\" id=\"old_order_" . $count . "\" value=\"" . $count . "\">";
+		$orderSelect .= "<input type=\"hidden\" id=\"start_order_" . $count . "\" value=\"" . $count . "\">";
 		$orderSelect .= "<input type=\"button\" value=\"Save Order\" onmousedown=\"submitPluginLoadOrder();\">";
 		return $orderSelect;
 	}
@@ -89,7 +90,7 @@ class Plugin_Organizer {
 			function uniqueOrder(currentId) {
 				var newVal = jQuery("#" + currentId).val();
 				var oldVal = jQuery("#old_" + currentId).val();
-				var selections = jQuery('select[name^=order]');
+				var selections = jQuery('select[name=order[]]');
 				for (var i=0; i<selections.length; i++) {
 					if (selections[i].id != currentId && selections[i].value == newVal) {
 						selections[i].value = oldVal;
@@ -100,29 +101,24 @@ class Plugin_Organizer {
 
 			}
 			function submitPluginLoadOrder(){
-				var selections = jQuery('select[name^=order]');
+				var selections = jQuery('select[name=order[]]');
 				alert("Your request is being processed");
-				var agentList = '';
+				var orderList = '';
+				var startOrderList = '';
 				for (var i=0; i<selections.length; i++) {
 					if (i == selections.length-1) {
-						agentList += selections[i].value;
+						orderList += selections[i].value;
+						startOrderList += jQuery("#start_" + selections[i].id).val();
 					} else {
-						agentList += selections[i].value + ",";
+						orderList += selections[i].value + ',';
+						startOrderList += jQuery("#start_" + selections[i].id).val() + ',';
 					}
 				}
-				if(navigator.appName == "Microsoft Internet Explorer") {
-				  Ajax = new ActiveXObject("Microsoft.XMLHTTP");
-				} else {
-				  Ajax = new XMLHttpRequest();
-				}
-				Ajax.open("GET", encodeURI(ajaxurl + '?action=plugin_organizer&agentList=' + agentList));
-				Ajax.onreadystatechange=function() {
-				  if(Ajax.readyState == 4) {
-					alert(Ajax.responseText);
-					document.body.removeChild(loadingDiv);
-				  }
-				}
-				Ajax.send(null);
+				
+				alert(orderList);
+				jQuery.post(encodeURI(ajaxurl + '?action=plugin_organizer'), { orderList: orderList, startOrder: startOrderList }, function (result) {
+					alert(result);
+				});
 			}
 		</script>
 		<?php
@@ -131,29 +127,13 @@ class Plugin_Organizer {
 	function PO_save_order() {
 		$returnStatus = "";
 		$plugins = get_option("active_plugins");
-		if (preg_match("/^(([0-9])+[,]*)*$/", $_GET['agentList'])) {
-			$newPlugArray = explode(",", $_GET['agentList']);
-			if (sizeof(array_unique($newPlugArray)) == sizeof($plugins)) {
-				$newPluginOrder = array();
-				$sorted = 0;
-				while($sorted == 0) {
-					$sorted = 1;
-					for($i=0; $i<sizeof($newPlugArray) - 1; $i++) {
-						if (isset($newPlugArray[$i+1]) && $newPlugArray[$i+1] < $newPlugArray[$i]) {
-							$sorted = 0;
-							$tmpVal = $newPlugArray[$i+1];
-							$newPlugArray[$i+1] = $newPlugArray[$i];
-							$newPlugArray[$i] = $tmpVal;
-							$tmpVal = $plugins[$i+1];
-							$plugins[$i+1] = $plugins[$i];
-							$plugins[$i] = $tmpVal;
-						}
-					}
-				}
-				foreach ($newPlugArray as $key=>$pos) {
-					$newPluginOrder[] = $plugins[$key];
-				}
-				update_option("active_plugins", $newPluginOrder);
+		if (preg_match("/^(([0-9])+[,]*)*$/", $_POST['orderList']) && preg_match("/^(([0-9])+[,]*)*$/", $_POST['startOrder'])) {
+			$newPlugArray = explode(",", $_POST['orderList']);
+			$startOrderArray = explode(",", $_POST['startOrder']);
+			if (sizeof(array_unique($newPlugArray)) == sizeof($plugins) && sizeof(array_unique($startOrderArray)) == sizeof($plugins)) {
+				array_multisort($startOrderArray, $newPlugArray);
+				array_multisort($newPlugArray, $plugins);
+				update_option("active_plugins", $plugins);
 				
 				$returnStatus = "The plugin load order has been changed.";
 			} else {
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: plugin order, sort plugin
 Requires at least: 2.8.2
 Tested up to: 3.0.3
-Stable tag: 0.1
+Stable tag: 0.1.1
 
 This plugin allows you to change the order that your plugins are loaded.
 
@@ -17,12 +17,15 @@ This plugin allows you to change the order that your plugins are loaded.
 1. Extract the downloaded Zip file.
 2. Upload the 'plugin_organizer' directory to the `/wp-content/plugins/` directory
 3. Activate the plugin through the 'Plugins' menu in WordPress
-4. There will be a new menu item under settings in the WordPress admin called Organize Plugins.
+4. You can either use the menu item under settings in the WordPress admin called Organize Plugins or there will be a drop down list for each plugin in the plugin admin page to select the load order.
 == Frequently Asked Questions ==
 
 == Screenshots ==
 
 == Changelog ==
+
+= 0.1.1 =
+improved the ajax requests on the plugin admin page.  
 
 = 0.1 =
 Initial version.
