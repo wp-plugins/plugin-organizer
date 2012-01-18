daaa4c65104b33add6c409bc5845e99b885c4147
foomagoo <foomagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-18 19:45:27 +0000

    Added ability to disable plugins in the admin using the Arbitrary URL admin page. Other fixes
    git-svn-id: https://plugins.svn.wordpress.org/plugin-organizer/trunk@491806 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/PluginOrganizerMU.class.php
+++ lib/PluginOrganizerMU.class.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Plugin Organizer MU
-Plugin URI: http://www.nebraskadigital.com/2010/12/27/plugin-organizer/
-Description: This is part of the Plugin Organizer plugin.  It enables the selective loading of plugins per post or page.
-Version: 0.9
+Plugin Name: Plugin Organizer
+Plugin URI: http://wpmason.com/plugin-organizer/
+Description: A plugin for specifying the load order of your plugins.
+Version: 1.0
 Author: Jeff Sterup
 Author URI: http://www.jsterup.com
 */
@@ -11,10 +11,10 @@ Author URI: http://www.jsterup.com
 	
 class PluginOrganizerMU {
 	function disable_plugins($pluginList) {
-		global $wpdb;
+		global $wpdb, $pagenow;
 		$newPluginList = array();
-		if (get_option("PO_disable_plugins") == "1" && !is_admin()) {
-			if (get_option("PO_version_num") != "0.9") {
+		if (get_option("PO_disable_plugins") == "1" && $pagenow != "plugins.php") {
+			if (get_option("PO_version_num") != "1.0" && !is_admin()) {
 				$newPluginList = $pluginList;
 				update_option("PO_disable_plugins", "0");
 			} else {
@@ -23,9 +23,10 @@ class PluginOrganizerMU {
 				$protocol = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
 				/*** return the full address ***/
 				$url = $protocol.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-				$preparedUrl = $wpdb->prepare($url);
-				$postPlugins = $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."PO_post_plugins WHERE permalink = '".$preparedUrl."'", ARRAY_A);
-				$urlPlugins = $wpdb->get_row("SELECT * FROM ".$wpdb->prefix."PO_url_plugins WHERE permalink = '".$preparedUrl."'", ARRAY_A);
+				$postPluginQuery = "SELECT * FROM ".$wpdb->prefix."PO_post_plugins WHERE permalink = %s";
+				$postPlugins = $wpdb->get_row($wpdb->prepare($postPluginQuery, $url), ARRAY_A);
+				$urlPluginQuery = "SELECT * FROM ".$wpdb->prefix."PO_url_plugins WHERE permalink = %s";
+				$urlPlugins = $wpdb->get_row($wpdb->prepare($urlPluginQuery, $url), ARRAY_A);
 				$disabledPostPlugins = unserialize($postPlugins['disabled_plugins']);
 				$enabledPostPlugins = unserialize($postPlugins['enabled_plugins']);
 				$disabledUrlPlugins = unserialize($urlPlugins['disabled_plugins']);
