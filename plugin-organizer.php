0e13fea827516138f7916aa21e58f6543412393b
foomagoo <foomagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-18 20:55:37 +0000

    Fixing missing db table error message when the table exists on windows server.
    git-svn-id: https://plugins.svn.wordpress.org/plugin-organizer/trunk@641311 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin-organizer.php
+++ plugin-organizer.php
@@ -3,7 +3,7 @@
 Plugin Name: Plugin Organizer
 Plugin URI: http://wpmason.com
 Description: A plugin for specifying the load order of your plugins.
-Version: 2.5.4
+Version: 2.5.5
 Author: Jeff Sterup
 Author URI: http://www.jsterup.com
 License: GPL2
