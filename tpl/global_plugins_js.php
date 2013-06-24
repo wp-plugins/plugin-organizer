c6ef6f6b2ea96f16649eff510f75ec98eb9ee8de
foomagoo <foomagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-24 14:18:16 +0000

    Fixed a problem with fuzzy url matching.  " characters were being added to the url so it would never match. Commented out the code that deleted the tables and added an option to the databse to prevent multiple imports.  Will add the delete code in a later version to clean up the tables after everyone is stable and has imported their settings. Added code to ensure that the old MU plugin is deleted before attempting to copy it from the lib directory.
    git-svn-id: https://plugins.svn.wordpress.org/plugin-organizer/tags/3.0.1@730910 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/global_plugins_js.php
@@ -0,0 +1,34 @@
+<?php
+global $wpdb;
+if ( current_user_can( 'activate_plugins' ) ) {
+	?>
+	<script type="text/javascript" language="javascript">
+		function PO_submit_global_plugins(){
+			var disabledList = new Array();
+			jQuery('.pluginsList').each(function() {
+				if (this.checked) {
+					disabledList[disabledList.length] = jQuery(this).val();
+				}
+			});
+			var revertHtml = jQuery('#pluginListdiv').html();
+			jQuery('#pluginListdiv').html('<div style="width: 100%;text-align: center;"><img src="<?php print $this->urlPath . "/image/ajax-loader.gif"; ?>"></div>');
+			
+			if (disabledList.length == 0) {
+				disabledList[0]="EMPTY";
+			}
+			jQuery.post(encodeURI(ajaxurl + '?action=PO_save_global_plugins'), { 'disabledList[]': disabledList, PO_nonce: '<?php print $this->nonce; ?>' }, function (result) {
+				alert(result);
+				jQuery('#pluginListdiv').html(revertHtml);
+				jQuery('.pluginsList').each(function() {
+					if (disabledList.indexOf(this.value) != -1) {
+						jQuery(this).attr('checked', true);
+					} else {
+						jQuery(this).attr('checked', false);
+					}
+				});
+			});
+		}
+	</script>
+	<?php
+}
+?>
