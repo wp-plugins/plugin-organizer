cccb399ed09e2dfb495929fd2bca7ad4cdef23a3
foomagoo <foomagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-19 21:24:28 +0000

    Moved function call to correct old group members from the init call to the admin_menu action.
    git-svn-id: https://plugins.svn.wordpress.org/plugin-organizer/tags/5.0.2@878267 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/common_js.php
@@ -0,0 +1,45 @@
+<script language="javascript" src="<?php print $this->urlPath; ?>/js/validation.js"></script>
+<script language="javascript" type="text/javascript">
+	function PO_toggle_all(toggleCheckbox, itemClass, buttonID, itemID) {
+		var toggle = jQuery("#"+toggleCheckbox).attr('checked');
+		jQuery("."+itemClass).each(function() {  
+			var splitID = this.id.split('_');
+			if (toggle) {
+				PO_set_on_off(buttonID+splitID[1], itemID+splitID[1], 1);
+			} else {
+				PO_set_on_off(buttonID+splitID[1], itemID+splitID[1], 0);
+			}
+		});  
+	}
+
+	function PO_toggle_on_off(buttonID, checkboxID) {
+		if (jQuery('#'+buttonID).hasClass('pluginsButtonOff')) {
+			PO_set_on_off(buttonID, checkboxID, 1);
+		} else {
+			PO_set_on_off(buttonID, checkboxID, 0);
+		}
+	}
+	
+	function PO_set_on_off(buttonID, checkboxID, onOff) {
+		if (onOff == 1) {
+			jQuery('#'+checkboxID).attr('checked', false);
+			jQuery('#'+buttonID).attr('src', '<?php print $this->urlPath; ?>/image/on-button.png');
+			jQuery('#'+buttonID).attr('alt', 'On');
+			jQuery('#'+buttonID).removeClass('pluginsButtonOff');
+			jQuery('#'+buttonID).addClass('pluginsButtonOn');
+		} else {
+			jQuery('#'+checkboxID).attr('checked', true);
+			jQuery('#'+buttonID).attr('src', '<?php print $this->urlPath; ?>/image/off-button.png');
+			jQuery('#'+buttonID).attr('alt', 'Off');
+			jQuery('#'+buttonID).removeClass('pluginsButtonOn');
+			jQuery('#'+buttonID).addClass('pluginsButtonOff');
+		}
+	}
+	
+	<?php
+	print "var regex = new Array();\n";
+	foreach ($this->regex as $key=>$val) {
+		print "regex['$key'] = $val;\n";
+	}
+	?>
+</script>
