fe7ff1e53897bdc61fa6b18e2ac67a56f5d4a02a
foomagoo <foomagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-08 15:21:16 +0000

    = 5.4 = Changed the function used from strpos to stripos for mobile browser string matching. Fixed an issue where site freezes during upgrade. Changed from using HTTP_HOST to the wordpress url for trailing slash correction.
    git-svn-id: https://plugins.svn.wordpress.org/plugin-organizer/tags/5.4@984443 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/admin_css.php
@@ -0,0 +1,90 @@
+<style type="text/css">
+	#icon-po-settings {
+		background: url("<?php print $this->urlPath; ?>/image/po-icon-32x32.png") no-repeat scroll 0px 0px transparent;
+	}
+	#icon-po-global {
+		float: left;
+		background: url("<?php print $this->urlPath; ?>/image/po-global-32x32.png") no-repeat scroll 0px 0px transparent;
+	}
+
+	.po-setting-icon {
+		float: left;
+		width: 32px;
+		height: 32px;
+		margin: 8px 5px 0px 0px;
+	}
+
+	.po-setting-title {
+		float: left;
+	}
+
+	.activePlugin {
+		color: #990033;
+	}
+
+	.badInputLabel {
+		color: #990033;
+		font-weight: bold;
+	}
+	.badInput {
+		background-color: #990033;
+	}
+	.metaBoxLabel {
+		font-size: 20px;
+		line-height: 22px;
+		padding: 5px;
+	}
+	.metaBoxContent {
+		border: 2px outset #000000;
+		margin-bottom: 20px;
+	}
+	.metaBoxContent input[type="checkbox"], .plugin-organizer_page_PO_global_plugins input[type="checkbox"], .plugin-organizer_page_PO_global_plugins input[type="radio"], .toplevel_page_Plugin_Organizer input[type="checkbox"], .toplevel_page_Plugin_Organizer input[type="radio"], .plugin-organizer_page_PO_url_admin input[type="checkbox"], .plugin-organizer_page_PO_url_admin input[type="radio"] {
+		margin: 0px 3px 0px 3px !important;
+	}
+
+	.poPermalinkInput, .poFilterNameInput {
+		width: 90%;
+		margin: 10px;
+	}
+
+	.pluginWrap, .groupWrap {
+		padding: 0px;
+		border-bottom: 1px solid #cccccc;
+		line-height: 26px;
+	}
+
+	.pluginsList, .mobilePluginsList, .pluginGroupList, .mobilePluginGroupList, #toggleAllPlugins, #toggleAllMobilePlugins, #toggleAllPluginGroups, #toggleAllMobilePluginGroups, #toggleAllGroups, #toggleAllMobileGroups {
+		display: none !important;
+	}
+	
+	.pluginsButton, .pluginGroupButton {
+		vertical-align: middle;
+		margin: 0px 3px;
+	}
+
+	.inactivePluginWrap, .inactivePluginGroupWrap {
+		background-color: #cccccc;
+		border-bottom: 1px solid #ffffff;
+	}
+
+	.toggleContainer {
+		width: 80px;
+		float: left;
+		text-align: center;
+		padding: 10px 5px;
+		border-right: 1px solid #000000;
+	}
+	
+	.pluginLabel {
+		padding: 10px 0px 10px 10px;
+		float: left;
+	}
+
+	.pluginListHead {
+		font-size: 18px !important;
+		border-bottom: 2px solid #000000;
+	}
+	.network-active {
+		background-color: #333333 !important;
+	}
+</style>
