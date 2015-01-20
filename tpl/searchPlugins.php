40f04444f1652456a9d913cf22111220dffc551b
foomagoo <foomagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 16:17:52 +0000

    Fixed a typo that prevented globally disabled mobile groups from being enabled on a post or page. Streamlined plugin matching functionality when saving enabled/disabled during meta box save so it all uses a single function instead of multiple if statements. Fixed the MU plugin so PO can't be disabled on the admin side which locks the user out of all plugin organizer settings.
    git-svn-id: https://plugins.svn.wordpress.org/plugin-organizer/tags/5.7.6@1071789 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/searchPlugins.php
@@ -0,0 +1,17 @@
+<div id="wrap">
+    <div class="po-setting-icon" id="icon-po-search"> <br /> </div>
+
+    <h2 class="po-setting-title">Search Plugins</h2>
+    <div style="clear: both;"></div>
+	<p>Select the plugins you would like to disable/enable on the search results page.</p>
+	<div id="poststuff" class="metabox-holder">
+      <div id="post-body">
+        <div id="post-body-content">
+	      <div id="pluginListdiv" class="stuffbox" style="width: 98%">
+              <? require_once('postMetaBox.php'); ?>
+			  <input type=button name=submit value="Save" onmousedown="PO_submit_search_plugins();">
+	    </div>
+      </div>
+    </div>
+</div>
+
